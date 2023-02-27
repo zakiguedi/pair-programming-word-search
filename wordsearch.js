f8586b96d1823dd0ffcff71cd7c90bf7b3dcab5b
zakiguedi <zaki.guedi@gmail.com>
2023-02-26 19:50:18 -0500

    pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
+const wordSearch = (letters, word) => {
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    let result = [];
+    for (let i = 0; i < letters[0].length; i++) {
+        const row = [];
+        for (let j = 0; j < letters.length; j++) {
+            row.push(letters[j][i]);
+        }
+        result.push(row);
+        let finalName = result.join('').replaceAll(',', '')
+        if (finalName.includes(word)) {
+            return true;
+        } else {
+            result = []
+        }
+    }
+    return false;
 }
 
 module.exports = wordSearch
